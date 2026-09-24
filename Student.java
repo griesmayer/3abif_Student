78331fe460988e813f8cd6d2410957a80fb6363c
Thomas Griesmayer <griesmayer@spengergasse.at>
2026-09-24 21:18:33 +0200

    add get/set

--- Student.java
+++ Student.java
@@ -3,4 +3,14 @@ public class Student
     private String  name;
     private int     alter;
     private boolean matura;
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String neuName)
+    {
+        name = neuName;
+    }
 }
